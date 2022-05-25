cf287721494a4976bcafb214a4a894051c655af7
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2022-05-25 14:35:16 +0200

    made a begin with decktray click and follow cursor

--- DecktrayApplication/wwwroot/scripts/main.js
+++ DecktrayApplication/wwwroot/scripts/main.js
@@ -1,3 +1,18 @@
-function hoi() {
-    console.log("Hoi");
+function hoi(myId) {
+    console.log(`Hoi ${myId}`);
 }
+
+var clickBool = 0;
+
+function clickDeckTray() {
+    clickBool++;
+    if (clickBool > 1) {
+        clickBool = 0;
+    }
+    console.log(`Clickbool = ${clickBool}`);
+}
+
+document.addEventListener('mousemove', function (ev) {
+    document.getElementById('followMe').style.transform = 'translateY(' + (ev.clientY - 770) + 'px)';
+    document.getElementById('followMe').style.transform += 'translateX(' + (ev.clientX - 305) + 'px)';
+}, false);
